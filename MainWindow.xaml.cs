09d183013ffa9823c5b48f699373874b92d324cc
Nick de Boer <denick15@gmail.com>
2022-09-27 10:35:39 +0200

    Fixed namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DesignPatterns
+namespace PaintPatterns
 {
     public partial class MainWindow : Window
     {
